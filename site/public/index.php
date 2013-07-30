905067f0f88d51517debf1e440aaec7e957098ae
Aaron Evans <aevans@ancestry.com>
2013-07-30 09:56:40 -0600

    index summary

---
+++ site/public/index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+	<title> Utah Startups</title>
+</head>
+<body>
+	<h1> A Resource for Utah Startups </h1>
+
+	<div> Subscribe </div>
+
+	<div> Add your company to the Utah Startups directory </div>
+
+	<div> Read startup profiles and interviews </div>
+</body>
+</html>
+
